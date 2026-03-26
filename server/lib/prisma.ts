3dff56c6e7bf98fb31a0f2bacbb54e9f6fe5c2fc
gabriel.berlucci <gabrielberlucci@proton.me>
2026-03-26 03:07:07 -0300

    feat: add lib import for prisma

---
+++ server/lib/prisma.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = process.env.DATABASE_URL!;
+
+const prismaClientSingleton = () => {
+  if (process.env.NODE_ENV === 'development') {
+    const adapter = new PrismaPg({ connectionString });
+    return new PrismaClient({ adapter });
+  }
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = prisma;
+}
+
+export { prisma };
